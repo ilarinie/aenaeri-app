208d81c5d1a03835823725d9bebacc500dc8b4ea
Ilari Nieminen <ilari.nieminen@codento.com>
2019-12-04 00:22:55 +0200

    fix be prod config

--- server/db/populateDatabase.ts
+++ server/db/populateDatabase.ts
@@ -2,7 +2,7 @@ import { initializeDB } from '.';
 
 import { populateDatabase } from './populator';
 
-process.env.DATABASE_URL = 'postgresql://aenaeri-app:password123@localhost:5432/aenaeri-app';
+// process.env.DATABASE_URL = 'postgresql://aenaeri-app:password123@localhost:5432/aenaeri-app';
 
 initializeDB().then(() => {
     populateDatabase();
